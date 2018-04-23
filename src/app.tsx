c2af8870eccd485a972801887311d96aa9f99e50
Gerald de Jong <geralddejong@gmail.com>
2018-04-23 13:38:17 +0200

    Some SVG appearing

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,12 @@
 import * as React from 'react';
 import './app.css';
 
-import logo from './logo.svg';
 import TokenView from './token-view';
 
 class App extends React.Component {
     public render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and wow to reload.
-                </p>
                 <TokenView/>
             </div>
         );
--- src/token-view.tsx
+++ src/token-view.tsx
@@ -1,15 +1,61 @@
 import * as React from 'react';
 
+import './token-view.css'
+import {HEXAGON_POINTS} from './patch-token/constants';
+
 class TokenView extends React.Component {
-  public render() {
-    return (
-      <div>
-        <header>
-          <h1>Patch Token</h1>
-        </header>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <svg className="TokenView" viewBox="-1,-1,2,2"
+                 width={window.innerWidth} height={window.innerHeight}>
+                <polygon points={HEXAGON_POINTS}/>
+            </svg>
+        );
+    }
 }
 
 export default TokenView;
+
+/*
+
+  get mainViewBox() {
+    return this.patch ? this.zoomViewBox ? this.zoomViewBox : this.patch.mainViewBox : '-1,-1,2,2';
+  }
+
+<svg [attr.viewBox]="mainViewBox" (click)="clickBackground()" class="main-panel">
+  <g *ngIf="selectedToken && !patch.isSingleToken" [attr.transform]="selectedToken.transform" [ngClass]="selectedTokenClassMap">
+    <polygon id="selected" [attr.points]="hexagonPoints"/>
+  </g>
+  <g *ngFor="let light of lights; let lightIndex=index;">
+    <g [ngClass]="patchViewClassMap(light)" (click)="clickLight(light, $event)"
+           (mouseenter)="lightEnter(light, true)" (mouseleave)="lightEnter(light, false)">
+      <polygon
+        [id]="'hex' + lightIndex"
+        [attr.transform]="light.transform"
+        [attr.points]="hexagonPoints"/>
+    </g>
+  </g>
+</svg>
+
+<div className="token-container">
+  <svg  xmlns:svg="http://www.w3.org/2000/svg" [attr.viewBox]="tokenViewBox" class="token-panel">
+    <g *ngFor="let frame of tokenFrames; let frameIndex = index;" (click)="clickToken(frame)">
+      <g *ngIf="frame" [attr.transform]="frame.transform">
+        <polygon
+          [ngClass]="{'token-background': frame.owner === owner, 'token-background-free': !frame.owner }"
+          [id]="'frame' + frameIndex"
+          transform="rotate(30) scale(1.47)"
+          [attr.points]="hexagonPoints"/>
+        <g *ngFor="let light of frame.lights; let lightIndex=index;">
+          <g [ngClass]="tokenViewClassMap(light)">
+            <polygon
+              [id]="'tlight' + lightIndex"
+              [attr.transform]="light.transform"
+              [attr.points]="hexagonPoints"/>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+</div>
+ */
