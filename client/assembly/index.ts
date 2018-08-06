3734e0b61a56f222b9c35e9d96e9c9b2e7f0ee71
Gerald de Jong <geralddejong@gmail.com>
2018-08-06 22:19:22 +0200

    Physics for wasm almost ready

--- client/assembly/index.ts
+++ client/assembly/index.ts
@@ -10,6 +10,13 @@ const MAX_FACES = 200;
 const JOINTS_OFFSET = 0;
 const INTERVALS_OFFSET = MAX_JOINTS * JOINT_SIZE;
 const FACES_OFFSET = INTERVALS_OFFSET + MAX_INTERVALS * INTERVAL_SIZE;
+const METADATA_OFFSET = FACES_OFFSET + MAX_FACES * FACE_SIZE;
+const JOINT_RADIUS = 0.15;
+const AMBIENT_JOINT_MASS = 0.1;
+const CABLE_MASS_FACTOR = 0.05;
+const SPRING_SMOOTH = 0.03;
+const BAR_SMOOTH = 0.6;
+const CABLE_SMOOTH = 0.01;
 
 let jointCount = 0;
 let intervalCount = 0;
@@ -65,10 +72,16 @@ function setZ(vPtr: u32, v: f64): void {
     store<f64>(vPtr, v, FLOAT_SIZE * 2);
 }
 
-function set(vPtr: u32, x: f64, y: f64, z: f64): void {
+function setAll(vPtr: u32, x: f64, y: f64, z: f64): void {
     setX(vPtr, x);
     setY(vPtr, y);
     setZ(vPtr, z);
+}
+
+function setVector(vPtr: u32, v: u32): void {
+    setX(vPtr, getX(v));
+    setY(vPtr, getY(v));
+    setZ(vPtr, getZ(v));
 }
 
 function zero(vPtr: u32): void {
@@ -160,17 +173,17 @@ function gravityPtr(jointIndex: u32): u32 {
     return jointPtr(jointIndex) + VECTOR_SIZE * 4;
 }
 
-function intervalMass(jointIndex: u32): f64 {
-    return getFloat(jointPtr(jointIndex) + VECTOR_SIZE * 5);
+function intervalMassPtr(jointIndex: u32): u32 {
+    return jointPtr(jointIndex) + VECTOR_SIZE * 5;
 }
 
-function altitude(jointIndex: u32): f64 {
-    return getFloat(jointPtr(jointIndex) + VECTOR_SIZE * 5 + FLOAT_SIZE);
+function altitudePtr(jointIndex: u32): u32 {
+    return jointPtr(jointIndex) + VECTOR_SIZE * 5 + FLOAT_SIZE;
 }
 
 function createJoint(x: f64, y: f64, z: f64): u32 {
     const jointIndex = jointCount++;
-    set(locationPtr(jointIndex), x, y, z);
+    setAll(locationPtr(jointIndex), x, y, z);
     zero(forcePtr(jointIndex));
     zero(velocityPtr(jointIndex));
     zero(gravityPtr(jointIndex));
@@ -263,7 +276,117 @@ function createFace(joint0Index: u32, joint1Index: u32, joint2Index: u32): u32 {
 
 // construction
 
-export function setAltitude(altitude: f64): void {
+function splitVectors(vectorPtr: u32, basisPtr: u32, projectionPtr: u32, howMuch: f64): void {
+    const agreement = dot(vectorPtr, basisPtr);
+    setVector(projectionPtr, basisPtr);
+    multiplyScalar(projectionPtr, agreement * howMuch);
+}
+
+const projectionPtr = METADATA_OFFSET;
+const alphaProjectionPtr = projectionPtr + VECTOR_SIZE;
+const omegaProjectionPtr = alphaProjectionPtr + VECTOR_SIZE;
+const gravPtr = omegaProjectionPtr + VECTOR_SIZE;
+const ELASTIC: f64 = 0.05;
+const airDrag: f64 = 0.0002;
+const airGravity: f64 = 0.0001;
+const landDrag: f64 = 50;
+const landGravity: f64 = 30;
+
+function iterate(): void {
+    for (let intervalIndex: i32 = 0; intervalIndex < intervalCount; intervalIndex++) {
+        elastic(intervalIndex);
+    }
+    for (let intervalIndex: i32 = 0; intervalIndex < intervalCount; intervalIndex++) {
+        smoothVelocity(intervalIndex, SPRING_SMOOTH);
+    }
+    for (let jointIndex: i32 = 0; jointIndex < jointCount; jointIndex++) {
+        exertJointPhysics(jointIndex);
+        addScaledVector(velocityPtr(jointIndex), forcePtr(jointIndex), 1.0 / getFloat(intervalMassPtr(jointIndex)));
+        zero(forcePtr(jointIndex));
+        add(velocityPtr(jointIndex), absorbVelocityPtr(jointIndex));
+        zero(absorbVelocityPtr(jointIndex));
+    }
+    for (let intervalIndex: i32 = 0; intervalIndex < intervalCount; intervalIndex++) {
+        const alphaAltitude = getFloat(altitudePtr(getAlphaIndex(intervalIndex)));
+        const omegaAltitude = getFloat(altitudePtr(getOmegaIndex(intervalIndex)));
+        const straddle = (alphaAltitude > 0 && omegaAltitude <= 0) || (alphaAltitude <= 0 && omegaAltitude > 0);
+        if (straddle) {
+            const totalAltitude = Math.abs(alphaAltitude) + Math.abs(omegaAltitude);
+            if (totalAltitude > 0.001) {
+                setVector(gravPtr, gravityPtr(getAlphaIndex(intervalIndex)));
+                lerp(gravPtr, gravityPtr(getOmegaIndex(intervalIndex)), Math.abs(omegaAltitude) / totalAltitude);
+            }
+            else {
+                addVectors(gravPtr, gravityPtr(getAlphaIndex(intervalIndex)), gravityPtr(getAlphaIndex(intervalIndex)));
+                multiplyScalar(gravPtr, 0.5);
+            }
+        }
+        else {
+            addVectors(gravPtr, gravityPtr(getAlphaIndex(intervalIndex)), gravityPtr(getAlphaIndex(intervalIndex)));
+            multiplyScalar(gravPtr, 0.5);
+        }
+        add(velocityPtr(getAlphaIndex(intervalIndex)), gravPtr);
+        add(velocityPtr(getOmegaIndex(intervalIndex)), gravPtr);
+    }
+    for (let jointIndex: i32 = 0; jointIndex < jointCount; jointIndex++) {
+        add(locationPtr(jointIndex), velocityPtr(jointIndex));
+        setFloat(intervalMassPtr(jointIndex), AMBIENT_JOINT_MASS);
+    }
+}
+
+
+function exertJointPhysics(jointIndex: u32): void {
+    const altitude = getY(locationPtr(jointIndex));
+    if (altitude > JOINT_RADIUS) {
+        exertGravity(jointIndex, airGravity);
+        multiplyScalar(velocityPtr(jointIndex), 1 - airDrag);
+    }
+    else if (altitude < -JOINT_RADIUS) {
+        exertGravity(jointIndex, -airGravity * landGravity);
+        multiplyScalar(velocityPtr(jointIndex), 1 - airDrag * landDrag);
+    }
+    else {
+        const degree = (altitude + JOINT_RADIUS) / (JOINT_RADIUS * 2);
+        const gravityValue = airGravity * degree + -airGravity * landGravity * (1 - degree);
+        exertGravity(jointIndex, gravityValue);
+        const drag = airDrag * degree + airDrag * landDrag * (1 - degree);
+        multiplyScalar(velocityPtr(jointIndex), 1 - drag);
+    }
+}
+
+function exertGravity(jointIndex: u32, value: f64): void {
+    const velocity = velocityPtr(jointIndex);
+    setY(velocity, getY(velocity) - value);
+}
+
+
+function smoothVelocity(intervalIndex: u32, degree: f64): void {
+    splitVectors(velocityPtr(getAlphaIndex(intervalIndex)), unitPtr(intervalIndex), alphaProjectionPtr, degree);
+    splitVectors(velocityPtr(getOmegaIndex(intervalIndex)), unitPtr(intervalIndex), omegaProjectionPtr, degree);
+    addVectors(projectionPtr, alphaProjectionPtr, omegaProjectionPtr);
+    multiplyScalar(projectionPtr, 0.5);
+    sub(absorbVelocityPtr(getAlphaIndex(intervalIndex)), alphaProjectionPtr);
+    sub(absorbVelocityPtr(getOmegaIndex(intervalIndex)), omegaProjectionPtr);
+    add(absorbVelocityPtr(getAlphaIndex(intervalIndex)), projectionPtr);
+    add(absorbVelocityPtr(getOmegaIndex(intervalIndex)), projectionPtr);
+}
+
+function elastic(intervalIndex: u32): void {
+    calculateSpan(intervalIndex);
+    const span = getFloat(spanPtr(intervalIndex));
+    const idealSpan = getFloat(idealSpanPtr(intervalIndex));
+    const stress = ELASTIC * (span - idealSpan) * idealSpan * idealSpan;
+    addScaledVector(forcePtr(getAlphaIndex(intervalIndex)), unitPtr(intervalIndex), stress / 2);
+    addScaledVector(forcePtr(getOmegaIndex(intervalIndex)), unitPtr(intervalIndex), -stress / 2);
+    const canPush = true;
+    const mass = canPush ? idealSpan * idealSpan * idealSpan : span * CABLE_MASS_FACTOR;
+    const alphaMass = intervalMassPtr(getAlphaIndex(intervalIndex));
+    setFloat(alphaMass, getFloat(alphaMass) + mass / 2);
+    const omegaMass = intervalMassPtr(getOmegaIndex(intervalIndex));
+    setFloat(omegaMass, getFloat(omegaMass) + mass / 2);
+}
+
+export function setFabricAltitude(altitude: f64): void {
     let lowest: f64 = 10000;
     for (let jointIndex: i32 = 0; jointIndex < jointCount; jointIndex++) {
         if (getY(jointPtr(jointIndex)) < lowest) {
@@ -307,7 +430,7 @@ function tetra(): void {
     createFace(1, 3, 2);
     createFace(1, 0, 3);
     createFace(2, 3, 0);
-    setAltitude(2);
+    setFabricAltitude(2);
 }
 
 // public tetraFace(face: Face): void {
@@ -329,135 +452,3 @@ function tetra(): void {
 //     this.faces.push(new Face([face.joints[2], face.joints[0], apexJoint]));
 // }
 
-// export const JOINT_RADIUS = 0.15;
-// const AMBIENT_JOINT_MASS = 0.1;
-// const CABLE_MASS_FACTOR = 0.05;
-// const SPRING_SMOOTH = 0.03;
-// // const BAR_SMOOTH = 0.6;
-// // const CABLE_SMOOTH = 0.01;
-//
-// export interface IPhysicsValue {
-//     name: string;
-//     value: number;
-// }
-//
-// export interface IConstraints {
-//     elastic: () => number;
-//     exertJointPhysics: (joint: Joint, fabric: Fabric) => void;
-// }
-//
-// export class Physics {
-//
-//     private static splitArrows(arrow: Vector3, basis: Vector3, projection: Vector3, howMuch: number) {
-//         const agreement = arrow.dot(basis);
-//         projection.set(basis.x, basis.y, basis.z);
-//         projection.multiplyScalar(agreement * howMuch);
-//     }
-//
-//     private gravity = new Vector3();
-//     private projection = new Vector3();
-//     private alphaProjection = new Vector3();
-//     private omegaProjection = new Vector3();
-//
-//     constructor(public constraints: IConstraints) {
-//     }
-//
-//     public iterate(fabric: Fabric) {
-//         fabric.intervals.forEach(interval => {
-//             this.elastic(interval);
-//         });
-//         fabric.intervals.forEach(interval => {
-//             this.smoothVelocity(interval, SPRING_SMOOTH);
-//         });
-//         fabric.joints.forEach(joint => {
-//             this.constraints.exertJointPhysics(joint, fabric);
-//             joint.velocity.addScaledVector(joint.force, 1.0 / joint.intervalMass);
-//             joint.force.set(0, 0, 0);
-//             joint.velocity.add(joint.absorbVelocity);
-//             joint.absorbVelocity.set(0, 0, 0);
-//         });
-//         fabric.intervals.forEach(interval => {
-//             const alphaAltitude = interval.alpha.altitude;
-//             const omegaAltitude = interval.omega.altitude;
-//             const straddle = (alphaAltitude > 0 && omegaAltitude <= 0) || (alphaAltitude <= 0 && omegaAltitude > 0);
-//             if (straddle) {
-//                 const totalAltitude = Math.abs(alphaAltitude) + Math.abs(omegaAltitude);
-//                 if (totalAltitude > 0.001) {
-//                     const g = interval.alpha.gravity;
-//                     this.gravity.set(g.x, g.y, g.z);
-//                     this.gravity.lerp(interval.omega.gravity, Math.abs(omegaAltitude) / totalAltitude);
-//                 }
-//                 else {
-//                     this.gravity.addVectors(interval.alpha.gravity, interval.omega.gravity).multiplyScalar(0.5);
-//                 }
-//             }
-//             else {
-//                 this.gravity.addVectors(interval.alpha.gravity, interval.omega.gravity).multiplyScalar(0.5);
-//             }
-//             interval.alpha.velocity.add(this.gravity);
-//             interval.omega.velocity.add(this.gravity);
-//         });
-//         fabric.joints.forEach(joint => {
-//             joint.location.add(joint.velocity);
-//             joint.intervalMass = AMBIENT_JOINT_MASS;
-//         });
-//     }
-//
-//     private elastic(interval: Interval) {
-//         interval.calculate();
-//         interval.stress = this.constraints.elastic()* (interval.span - interval.idealSpan) * interval.idealSpan * interval.idealSpan;
-//         interval.alpha.force.addScaledVector(interval.unit, interval.stress / 2);
-//         interval.omega.force.addScaledVector(interval.unit, -interval.stress / 2);
-//         const canPush = true;
-//         const mass = canPush ? interval.idealSpan * interval.idealSpan * interval.idealSpan : interval.span * CABLE_MASS_FACTOR;
-//         interval.alpha.intervalMass += mass / 2;
-//         interval.omega.intervalMass += mass / 2;
-//     }
-//
-//     private smoothVelocity(interval: Interval, degree: number) {
-//         Physics.splitArrows(interval.alpha.velocity, interval.unit, this.alphaProjection, degree);
-//         Physics.splitArrows(interval.omega.velocity, interval.unit, this.omegaProjection, degree);
-//         this.projection.addVectors(this.alphaProjection, this.omegaProjection).multiplyScalar(0.5);
-//         interval.alpha.absorbVelocity.sub(this.alphaProjection);
-//         interval.omega.absorbVelocity.sub(this.omegaProjection);
-//         interval.alpha.absorbVelocity.add(this.projection);
-//         interval.omega.absorbVelocity.add(this.projection);
-//     }
-//
-// }
-
-// export class VerticalConstraints implements IConstraints {
-//
-//     private static exertGravity(joint: Joint, value: number) {
-//         joint.velocity.y -= value;
-//     }
-//
-//     public airDrag: IPhysicsValue = {name: "airDrag", value: 0.0002};
-//     public airGravity: IPhysicsValue = {name: "airGravity", value: 0.0001};
-//     public landDrag: IPhysicsValue = {name: "landDrag", value: 50};
-//     public landGravity: IPhysicsValue = {name: "landGravity", value: 30};
-//     public elasticFactor: IPhysicsValue = {name: "elasticFactor", value: 0.05};
-//
-//     public exertJointPhysics(joint: Joint, fabric: Fabric) {
-//         const altitude = joint.location.y;
-//         if (altitude > JOINT_RADIUS) {
-//             VerticalConstraints.exertGravity(joint, this.airGravity.value);
-//             joint.velocity.multiplyScalar(1 - this.airDrag.value);
-//         }
-//         else if (altitude < -JOINT_RADIUS) {
-//             VerticalConstraints.exertGravity(joint, -this.airGravity.value * this.landGravity.value);
-//             joint.velocity.multiplyScalar(1 - this.airDrag.value * this.landDrag.value);
-//         }
-//         else {
-//             const degree = (altitude + JOINT_RADIUS) / (JOINT_RADIUS * 2);
-//             const gravityValue = this.airGravity.value * degree + -this.airGravity.value * this.landGravity.value * (1 - degree);
-//             VerticalConstraints.exertGravity(joint, gravityValue);
-//             const drag = this.airDrag.value * degree + this.airDrag.value * this.landDrag.value * (1 - degree);
-//             joint.velocity.multiplyScalar(1 - drag);
-//         }
-//     }
-//
-//     public elastic = () => {
-//         return this.elasticFactor.value;
-//     };
-// }
