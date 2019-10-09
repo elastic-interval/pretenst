88a53a4c3fa1719a3fc4f648b4b4ba67a05eabe7
Mitchell Justin <8631205+mitchelljustin@users.noreply.github.com>
2019-10-09 12:47:29 -0400

    Scripty 2

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,16 @@ yarn install
 build_app() {
   APP=$1
   echo "Building $APP.."
-  yarn run build:fabric-engine
   yarn run build:"$APP"
   rm -rf /home/galapagotchi/www/"$APP"
-  cp build /home/galapagotchi/www/"$APP"
+  cp -r build /home/galapagotchi/www/"$APP"
 }
 
-# Build Pretenst
+# Build fabric engine, used by both apps
+yarn run build:fabric-engine
+
+# Build apps
 build_app pretenst
-# Build Galapagotchi
 build_app galapagotchi
 
 # Launch Galapagotchi backend server
