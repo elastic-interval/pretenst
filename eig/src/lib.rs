32b3d5792545427ee695158a6b9456645c4fc3e3
Gerald de Jong <geralddejong@gmail.com>
2019-12-26 15:08:22 +0000

    First rust experiments.

---
+++ eig/src/lib.rs
@@ -0,0 +1,19 @@
+extern crate vec3;
+
+#[wasm_bindgen]
+fn gumby() -> [i32; 3] {
+    let a = [1, 1, 1];
+    let b = [1, 1, 1];
+    let mut out = vec3::new(0, 0, 0);
+
+    vec3::add(&mut out, &a, &b);
+    out
+}
+
+#[cfg(test)]
+#[test]
+fn it_works() {
+    let pokey = vec3::new(2, 2, 2);
+    assert_eq!(gumby(), pokey);
+}
+
