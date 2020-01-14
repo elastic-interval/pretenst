fb8317d6af755f7d6cceb5ed49ca0526036659f5
Mitchell Justin <8631205+mitchelljustin@users.noreply.github.com>
2020-01-14 14:51:25 +0000

    REMOVE GALAPAGOTCHI
    - Remove galapagotchi server stuff
    - Remove travis
    - Rewrite deploy.sh to build & publish to Github Pages (pretenst.com)

--- galapagotchi-server/scripts/sql/users.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public."user" (id, "twitterProfileId", "isAdmin") VALUES (2, '40', false);
-INSERT INTO public."user" (id, "twitterProfileId", "isAdmin") VALUES (4, '30', false);
-INSERT INTO public."user" (id, "twitterProfileId", "isAdmin") VALUES (5, '5', false);
-INSERT INTO public."user" (id, "twitterProfileId", "isAdmin") VALUES (6, '1', false);
-INSERT INTO public."user" (id, "twitterProfileId", "isAdmin") VALUES (7, '7', false);
-INSERT INTO public."user" (id, "twitterProfileId", "isAdmin") VALUES (8, '57', false);
-INSERT INTO public."user" (id, "twitterProfileId", "isAdmin") VALUES (3, '42', true);
-INSERT INTO public."user" (id, "twitterProfileId", "isAdmin") VALUES (1, '12', true);
