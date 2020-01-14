4ba5e4ccfef7a25208f00e15f638f0d05c704598
Mitchell Justin <8631205+mitchelljustin@users.noreply.github.com>
2020-01-14 15:32:43 +0000

    Fix deploy.sh : dont deploy if nothing new

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,16 @@
 
 set -x
 
-cd client
+cd client || exit
 yarn build:fabric-engine
 yarn build
 
-cd ../../pretenst-com
+cd ../../pretenst-com || exit
 rm -rf app/*
 cp -r ../pretenst/client/build/* app/
+if [ -z "$(git diff)" ]; then
+  echo "App bundle hasn't changed, skipping deployment"
+  exit 0
+fi
 git add -A
 git commit -m "Publish app - $(date)"
-
-#
