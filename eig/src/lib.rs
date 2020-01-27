9fc79b42e8db4055b429efb9ade5b8f6aa1325fd
Gerald de Jong <geralddejong@gmail.com>
2020-01-27 12:16:54 +0000

    returning a float seems to be exported

--- eig/src/lib.rs
+++ eig/src/lib.rs
@@ -3,19 +3,19 @@ extern crate vec3;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn gumby() -> [i32; 3] {
-    let a = [1, 1, 1];
-    let b = [1, 1, 1];
-    let mut out = vec3::new(0, 0, 0);
+pub fn gumby() -> f32 {
+    let a: [f32; 3] = [1.0, 1.0, 1.0];
+    let b: [f32; 3] = [1.0, 1.0, 1.0];
+    let mut out: [f32; 3] = vec3::new(0.0, 0.0, 0.0);
 
     vec3::add(&mut out, &a, &b);
-    out
+    vec3::len(&out)
 }
 
 #[cfg(test)]
 #[test]
 fn it_works() {
-    let pokey = vec3::new(2, 2, 2);
-    assert_eq!(gumby(), pokey);
+    let pokey: [f32; 3] = vec3::new(2.0, 2.0, 2.0);
+    assert_eq!(gumby(), vec3::len(&pokey));
 }
 
