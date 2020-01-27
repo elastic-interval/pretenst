647fff07a51d2483fc77434ce2a6732e45fb11a1
Gerald de Jong <geralddejong@gmail.com>
2020-01-27 17:44:02 +0000

    Some more API faked.

--- eig/src/lib.rs
+++ eig/src/lib.rs
@@ -1,21 +1,65 @@
-extern crate vec3;
-
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn gumby() -> f32 {
-    let a: [f32; 3] = [1.0, 1.0, 1.0];
-    let b: [f32; 3] = [1.0, 1.0, 1.0];
-    let mut out: [f32; 3] = vec3::new(0.0, 0.0, 0.0);
-
-    vec3::add(&mut out, &a, &b);
-    vec3::len(&out)
+pub enum SurfaceCharacter {
+    Frozen,
+    Sticky,
+    Slippery,
+    Bouncy,
 }
+
+#[wasm_bindgen]
+pub fn init() -> usize {
+    10101
+}
+
+#[wasm_bindgen]
+pub fn set_surface_character(character: SurfaceCharacter) -> ()  {
+    unsafe {
+        SURFACE_CHARACTER = character
+    }
+}
+
+#[wasm_bindgen]
+pub fn set_push_and_pull(value: bool) -> () {
+    unsafe {
+        PUSH_AND_PULL = value
+    }
+}
+
+#[wasm_bindgen]
+pub fn set_coloring(pushes: bool, pulls: bool) -> () {
+    unsafe {
+        COLOR_PUSHES = pushes;
+        COLOR_PULLS = pulls;
+    }
+}
+
+#[wasm_bindgen]
+pub fn set_instance(index: u16) -> () {
+    unsafe {
+        INSTANCE_NUMBER = index;
+    }
+}
+
+#[wasm_bindgen]
+pub fn clone_instance(_from_index: u16, _index: u16) -> () {
+}
+
+#[wasm_bindgen]
+pub fn get_instance_count() -> u16 {
+    1
+}
+
+static mut SURFACE_CHARACTER: SurfaceCharacter = SurfaceCharacter::Frozen;
+static mut PUSH_AND_PULL: bool = true;
+static mut COLOR_PUSHES: bool = true;
+static mut COLOR_PULLS: bool = true;
+static mut INSTANCE_NUMBER: u16 = 0;
 
 #[cfg(test)]
 #[test]
 fn it_works() {
-    let pokey: [f32; 3] = vec3::new(2.0, 2.0, 2.0);
-    assert_eq!(gumby(), vec3::len(&pokey));
+    assert_eq!(init(), 10101);
 }
 
