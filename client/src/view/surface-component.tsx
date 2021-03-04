2d0f1075cca53ecfc459aee931fd5e014f11137a
Gerald de Jong <geralddejong@gmail.com>
2021-03-04 16:17:41 +0100

    BufferGeometry only

--- client/src/view/surface-component.tsx
+++ client/src/view/surface-component.tsx
@@ -7,15 +7,13 @@ import * as React from "react"
 import { useMemo } from "react"
 import { BufferAttribute, BufferGeometry, Color, DoubleSide, MeshPhongMaterial, Vector3 } from "three"
 
-const SURFACE = new MeshPhongMaterial({
-    color: new Color("#101010"),
+const MATERIAL = new MeshPhongMaterial({
+    color: new Color("#090909"),
     side: DoubleSide,
-    transparent: true,
-    opacity: 0.5,
 })
-export const KINDA = 0.866
-export const SURFACE_SCALE = 20
-export const HEXAGON_POINTS = [
+const KINDA = 0.866
+const SURFACE_SCALE = 20
+const HEXAGON_POINTS = [
     new Vector3(0, 0, -SURFACE_SCALE),
     new Vector3(-KINDA * SURFACE_SCALE, 0, -SURFACE_SCALE / 2),
     new Vector3(-KINDA * SURFACE_SCALE, 0, SURFACE_SCALE / 2),
@@ -24,48 +22,36 @@ export const HEXAGON_POINTS = [
     new Vector3(KINDA * SURFACE_SCALE, 0, -SURFACE_SCALE / 2),
     new Vector3(),
 ]
-export const SURFACE_LAND_COLOR = new Color("tan")
-export const SIX = 6
-export const UP = new Vector3(0, 1, 0)
-export const LAND_NORMAL_SPREAD = 0.01
+const TRIANGLES = [
+    6, 0, 1,
+    6, 1, 2,
+    6, 2, 3,
+    6, 3, 4,
+    6, 4, 5,
+    6, 5, 0,
+]
+const TRIANGLE_POSITION = TRIANGLES.map(t => HEXAGON_POINTS[t])
 
 export function SurfaceComponent(): JSX.Element {
     const geometry = useMemo(() => patchesGeometry(), [])
-    return <mesh name="Patches" geometry={geometry} material={SURFACE}/>
+    return <mesh name="Patches" geometry={geometry} material={MATERIAL}/>
 }
 
 function patchesGeometry(): BufferGeometry {
     const geometry = new BufferGeometry()
-    const positionF32 = new Float32Array(HEXAGON_POINTS.length * 3)
-    HEXAGON_POINTS.forEach((point, index) => {
+    const positionF32 = new Float32Array(TRIANGLE_POSITION.length * 3)
+    const normalF32 = new Float32Array(TRIANGLE_POSITION.length * 3)
+    TRIANGLE_POSITION.forEach((position, index) => {
         const faceOffset = index * 3
-        positionF32[faceOffset] = point.x
-        positionF32[faceOffset + 1] = point.y - 0.01
-        positionF32[faceOffset + 2] = point.z
+        positionF32[faceOffset] = position.x
+        positionF32[faceOffset + 1] = position.y - 0.01
+        positionF32[faceOffset + 2] = position.z
+        const normal = new Vector3(0,50,0).add(position).normalize()
+        normalF32[faceOffset] = normal.x
+        normalF32[faceOffset + 1] = normal.y
+        normalF32[faceOffset + 2] = normal.z
     })
-    const normalF32 = new Float32Array(SIX * 3 * 3)
-    const colorF32 = new Float32Array(SIX * 3 * 3)
-    const indexU16 = new Uint16Array(SIX * 3)
-    for (let a = 0; a < SIX; a++) {
-        const b = (a + 1) % SIX
-        const vertexNormals = [
-            UP,
-            new Vector3().add(UP).addScaledVector(HEXAGON_POINTS[a], LAND_NORMAL_SPREAD).normalize(),
-            new Vector3().add(UP).addScaledVector(HEXAGON_POINTS[b], LAND_NORMAL_SPREAD).normalize(),
-        ]
-        const faceOffset = a * 3
-        vertexNormals.forEach((normal, normalIndex) => {
-            const normalOffset = faceOffset + normalIndex * 3
-            normalF32[normalOffset] = normal.x
-            normalF32[normalOffset + 1] = normal.y
-            normalF32[normalOffset + 2] = normal.z
-        })
-        indexU16[faceOffset] = SIX
-        indexU16[faceOffset+1] = a
-        indexU16[faceOffset+2] = b
-    }
     geometry.setAttribute("position", new BufferAttribute(positionF32, 3))
     geometry.setAttribute("normal", new BufferAttribute(normalF32, 3))
-    geometry.setAttribute("color", new BufferAttribute(colorF32, 3))
     return geometry
 }
