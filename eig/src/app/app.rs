8c7d4cb04f6168219c9b018e156e42105ea50457
Mitchell Justin <8631205+mitchelljustin@users.noreply.github.com>
2022-10-14 11:03:00 +0200

    Somet stuff

--- eig/src/app/app.rs
+++ eig/src/app/app.rs
@@ -1,141 +1,133 @@
-use std::time::{Duration};
-
-use chrono::{DateTime, Utc};
-use egui_demo_lib::syntax_highlighting::{highlight, CodeTheme};
+use three_d::*;
 
 use crate::tenscript;
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct App {
     code: String,
     output: String,
-
-    #[serde(skip)]
-    last_saved: Option<DateTime<Utc>>,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self {
-            // Example stuff:
-            code: "(fabric)".into(),
-            output: String::new(),
-            last_saved: None,
-        }
-    }
+struct RenderState {
+    context: Context,
+    camera: Camera,
+    cpu_mesh: CpuMesh,
+    model: Gm<Mesh, ColorMaterial>,
+    gui: GUI,
+    viewport_zoom: f64,
 }
 
 impl App {
-    /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customized the look at feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+    pub fn new() -> Self {
+        Self {
+            // Example stuff:
+            code: "(fabric)".into(),
+            output: "".into(),
         }
-
-        Default::default()
     }
-}
+    pub fn run(mut self) {
+        let window = Window::new(WindowSettings {
+            title: "Pretenst".into(),
+            max_size: Some((1600, 1920)),
+            ..Default::default()
+        })
+            .unwrap();
+        let context = window.gl();
 
-impl eframe::App for App {
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self {
-            code,
-            last_saved,
-            output,
-        } = self;
+        let camera = Camera::new_perspective(
+            window.viewport(),
+            vec3(0.0, 0.0, 1.3),
+            vec3(0.0, 0.0, 0.0),
+            vec3(0.0, 1.0, 0.0),
+            degrees(45.0),
+            0.1,
+            10.0,
+        );
 
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        let cpu_mesh = CpuMesh {
+            positions: Positions::F32(vec![
+                vec3(0.5, -0.5, 0.0),  // bottom right
+                vec3(-0.5, -0.5, 0.0), // bottom left
+                vec3(0.0, 0.5, 0.0),   // top
+            ]),
+            colors: Some(vec![
+                Color::new(255, 0, 0, 255), // bottom right
+                Color::new(0, 255, 0, 255), // bottom left
+                Color::new(0, 0, 255, 255), // top
+            ]),
+            ..Default::default()
+        };
 
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.close();
-                    }
+        let model: Gm<Mesh, ColorMaterial> =
+            Gm::new(Mesh::new(&context, &cpu_mesh), ColorMaterial::default());
+
+        let gui = GUI::new(&context);
+        let viewport_zoom = 1.0;
+
+        let mut render_state = RenderState {
+            context,
+            camera,
+            cpu_mesh,
+            model,
+            gui,
+            viewport_zoom,
+        };
+
+        window.render_loop(move |frame_input| self.render(&mut render_state, frame_input));
+    }
+
+    pub(crate) fn render_side_panel(&mut self, ui: &mut egui::Ui) {
+        egui::warn_if_debug_build(ui);
+    }
+
+    fn render(&mut self, render_state: &mut RenderState, mut frame_input: FrameInput) -> FrameOutput {
+        // Ensure the viewport matches the current window viewport which changes if the window is resized
+        let RenderState {
+            camera,
+            model,
+            gui,
+            ..
+        } = render_state;
+
+
+        let mut panel_width = 0.0;
+        camera.set_viewport(frame_input.viewport);
+        gui.update(
+            &mut frame_input.events,
+            frame_input.accumulated_time,
+            frame_input.device_pixel_ratio,
+            |gui_context| {
+                use three_d::egui::*;
+                SidePanel::left("side_panel").show(gui_context, |ui| {
+                    use three_d::egui::*;
+                    ui.heading("Debug Panel");
                 });
-            });
+                panel_width = gui_context.used_size().x as f64;
+            },
+        );
+
+        // Set the current transformation of the triangle
+        model.set_transformation(Mat4::from_angle_y(radians(
+            (frame_input.accumulated_time * 0.005) as f32,
+        )));
+
+        camera.set_viewport(Viewport {
+            x: (panel_width * frame_input.device_pixel_ratio) as i32,
+            y: 0,
+            width: frame_input.viewport.width
+                - (panel_width * frame_input.device_pixel_ratio) as u32,
+            height: frame_input.viewport.height,
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.set_min_width(350.);
-            ui.heading("Pretenst");
 
-            match last_saved {
-                None => ui.label("Last saved at: Never"),
-                Some(time) => ui.label(format!("Last saved at: {time}")),
-            };
-            ui.add_space(10.0);
+        // Get the screen render target to be able to render something on the screen
+        frame_input
+            .screen()
+            // Clear the color and depth of the screen render target
+            .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
+            // Render the triangle with the color material which uses the per vertex colors defined at construction
+            .render(&camera, &[&model], &[]);
 
-            ui.label("Tenscript: ");
-
-            let mut layouter = |ui: &egui::Ui, code: &str, wrap_width: f32| {
-                let mut layout_job = highlight(ui.ctx(), &CodeTheme::dark(), code, "clj");
-                layout_job.wrap.max_width = wrap_width;
-                ui.fonts().layout_job(layout_job)
-            };
-            let code_editor = ui.add(
-                egui::TextEdit::multiline(code)
-                    .font(egui::TextStyle::Monospace) // for cursor height
-                    .code_editor()
-                    .desired_rows(10)
-                    .lock_focus(true)
-                    .desired_width(f32::INFINITY)
-                    .layouter(&mut layouter),
-            );
-            if code_editor.changed() {
-                *output = match tenscript::parse(code) {
-                    Ok(fabric_plan) => format!("{fabric_plan:#?}"),
-                    Err(error) => format!("{error:#?}"),
-                };
-            }
-            ui.add_space(20.);
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                let mut layouter = |ui: &egui::Ui, code: &str, wrap_width: f32| {
-                    let mut layout_job = highlight(ui.ctx(), &CodeTheme::dark(), code, "rs");
-                    layout_job.wrap.max_width = wrap_width;
-                    ui.fonts().layout_job(layout_job)
-                };
-                ui.add(
-                    egui::TextEdit::multiline(output)
-                        .font(egui::TextStyle::Monospace)
-                        .code_editor()
-                        .desired_rows(40)
-                        .lock_focus(false)
-                        .desired_width(f32::INFINITY)
-                        .layouter(&mut layouter),
-                );
-            });
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {});
-
-            egui::warn_if_debug_build(ui);
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            //TODO
-        });
-    }
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        self.last_saved = Some(Utc::now());
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
-    fn auto_save_interval(&self) -> Duration {
-        Duration::from_secs(5)
+        // Returns default frame output to end the frame
+        FrameOutput::default()
     }
 }
--- eig/src/main.rs
+++ eig/src/main.rs
@@ -1,37 +1,7 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 
 use eig::app::App;
 
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-    tracing_subscriber::fmt::init();
-
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Pretenst",
-        native_options,
-        Box::new(|cc| Box::new(App::new(cc))),
-    );
-}
-
-// when compiling to web using trunk.
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    // Make sure panics are logged using `console.error`.
-    console_error_panic_hook::set_once();
-
-    // Redirect tracing to console.log and friends:
-    tracing_wasm::set_as_global_default();
-
-    let web_options = eframe::WebOptions::default();
-    eframe::start_web(
-        "main_canvas", // hardcode it
-        web_options,
-        Box::new(|cc| Box::new(App::new(cc))),
-    )
-        .expect("failed to start eframe");
+    App::new().run();
 }
